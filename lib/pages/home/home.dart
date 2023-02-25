77960cf63b9978568eb4616417d85eaf12fd335e
Abhinav Chawla <abhinavchawla07@gmail.com>
2023-02-26 00:59:40 +0530

    Add random words service

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:synchronize/constants.dart' as app_constants;
 import 'package:synchronize/pages/about/about_page.dart';
 import 'package:synchronize/pages/home/components/body.dart';
+import 'package:synchronize/services/random_words_service.dart';
 
 class HomePage extends StatelessWidget {
   static String pageRoute = "/home";
@@ -26,6 +27,8 @@ class HomePage extends StatelessWidget {
         word_2: "abbaabbbaaacccbbbaaaabbaabbbaaa",
         score: '50',
         onSubmitPressed: (String word) {
+          String newWord = RandomWordsService.getNewWord("alley", "cat", word);
+          print("new word is $newWord");
         },
       ),
       resizeToAvoidBottomInset: false,
